5223462f5a6a267a8c34b4357417dd6fa94ff9f9
avibhardwaj243 <avibhardwaj243@gmail.com>
2018-08-19 20:42:27 +0530

    master : all crud operation done with Express_node_mongo

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const product = require('./routes/product.route'); // Imports routes for the pro
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+
+const dev_db_url = 'mongodb://root:root_123@ds121262.mlab.com:21262/productstutorial';
+//mongodb://root:root@123@ds123619.mlab.com:23619/productstutorial';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+//console.log(mongoDB);return false;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/products', product);
 
 var port = 1234;
--- routes/product.route.js
+++ routes/product.route.js
@@ -7,4 +7,11 @@ const product_controller = require('../controllers/product.controller');
 //a simple test url to check
 router.get('/test', product_controller.test);
 
+//CRUD
+router.post('/create', product_controller.product_create);//C
+router.get('/:id', product_controller.product_details);//R
+router.put('/:id/update', product_controller.product_update);//U
+router.delete('/:id/delete', product_controller.product_delete);//D
+
+
 module.exports = router;
